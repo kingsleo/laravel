76fdf3fea4a9e6238e2500f31247dfb89a17e016
kingsleo <76727998@qq.com>
2016-11-24 14:06:12 +0800

    commit taskcontroller

--- resources/views/tasks/section1.blade.php
+++ resources/views/tasks/section1.blade.php
@@ -11,8 +11,7 @@
 @stop
 
 @section("content")
-    content
-    <!-- 模板中输出php变量 -->
+    {{--<!-- 模板中输出php变量 -->
     <p>{{ $name }}</p>
     <!-- 模板中调用php代码 -->
     <p>{{ time() }}</p>
@@ -21,9 +20,41 @@
     <p>{{ var_dump($arr) }}</p>
     <p>{{ isset($name) ? $name:"default" }}</p>
     <p>{{ $name or "default" }}</p>
+    <!-- 原样输出 -->
+    <p>@{{ $name }}</p>
+    --}}{{-- 模板中的注释 --}}{{--
+    --}}{{-- 引入子视图 --}}{{--
+    @include('tasks.common1',['name'=>$name])--}}
 
-    {{-- 模板中的注释 --}}
-
-    {{-- 引入子视图 --}}
-    @include('tasks.common1',['name'=>$name])
+    @if($name == "kingsleo")
+        i'm kingsleo
+    @elseif($name=="zly")
+        i'm zly
+    @else
+        who am i
+    @endif
+    <br>
+    @if(in_array($name,$arr))
+        true
+    @else
+        false
+    @endif
+    <br>
+    @unless($name != "kingsleo")
+        {{ $name }}
+    @endunless
+    <br>
+    @for($i=1;$i<10;$i++)
+        {{ $i }}
+    @endfor
+    <br>
+    @foreach($tasks as $task)
+        {{ $task->name }}
+    @endforeach
+    <br>
+    @forelse($tasks as $task)
+        {{ $task->name }}
+    @empty
+        null
+    @endforelse
 @stop
